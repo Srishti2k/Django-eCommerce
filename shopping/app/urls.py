40afe7758da6253f9298dd083aaf9dc79e139aae
Srishti <srish592000@gmail.com>
2021-06-24 16:30:15 +0530

    render particular id

--- shopping/app/urls.py
+++ shopping/app/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from app import views
 urlpatterns = [
     #path('', views.home),
-
-    path('' , views.ProductView.as_view() , name = 'home'),
-    path('product-detail/', views.product_detail, name='product-detail'),
+    #path('product-detail/', views.product_detail, name='product-detail'),
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
@@ -15,4 +13,8 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('registration/', views.customerregistration, name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
+
+    #class based views
+    path('' , views.ProductView.as_view() , name = 'home'),    
+    path('product-detail/<int:pk>' , views.product_detail.as_view() , name = 'product-detail'),     
 ]
--- shopping/app/views.py
+++ shopping/app/views.py
@@ -14,8 +14,16 @@ class ProductView(View):
         context = {'topwear' : topwear , 'bottomwear' : bottomwear , 'mobile' : mobile , 'laptop' : laptop}
 
         return render(request , 'app/home.html' , context)
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+
+
+# def product_detail(request):
+#  return render(request, 'app/productdetail.html')
+
+class product_detail(View):
+    def get(self, request , pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html' , {'product' : product})
+
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
