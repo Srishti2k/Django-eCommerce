c12b21b0fd8e5208f97b1f15375442148465df4d
Srishti <srish592000@gmail.com>
2021-07-09 02:27:03 +0530

    address profile

--- shopping/app/urls.py
+++ shopping/app/urls.py
@@ -1,28 +1,41 @@
+from django.contrib import auth
 from app.forms import LoginForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.urls import path
 from app import views
 from django.contrib.auth import logout, views as auth_views
-from .forms import LoginForm
+from .forms import *
 urlpatterns = [
-    #path('', views.home),
-    #path('product-detail/', views.product_detail, name='product-detail'),
+  
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
-    path('profile/', views.profile, name='profile'),
+    
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
     path('changepassword/', views.change_password, name='changepassword'),
     path('mobile/>', views.mobile, name='mobile'),
-    #path('login/', views.login, name='login'),
-    #path('registration/', views.customerregistration, name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
-    path('accounts/login' , auth_views.LoginView.as_view(template_name='app/login.html' , authentication_form= LoginForm ), name='login'),
-    path('logout/' , auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('' , views.ProductView.as_view() , name = 'home'),  #class based 
+    path('product-detail/<int:pk>' , views.product_detail.as_view() , name = 'product-detail'), #class based 
+    
 
 
-    #class based views
-    path('' , views.ProductView.as_view() , name = 'home'),    
-    path('product-detail/<int:pk>' , views.product_detail.as_view() , name = 'product-detail'),     
-    path('registration/', views.customerregistration.as_view() , name='customerregistration')
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    
+
+
+
+
+
+
+
+    #login logout registration
+    path('accounts/login' , auth_views.LoginView.as_view(template_name='app/login.html' , authentication_form= LoginForm ), name='login'), #BUILTIN login view
+    path('logout/' , auth_views.LogoutView.as_view(next_page='login'), name='logout'), #BUILTIN Logout view
+    path('registration/', views.customerregistration.as_view() , name='customerregistration'), #class based 
+    #password change
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone/'), name='passwordchange'),
+
+    path('passwordchangedone/', auth_views.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone')
 ]
+
--- shopping/app/views.py
+++ shopping/app/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.contrib import messages
 from .models import *
 from .forms import *
+
 # def home(request):
 #  return render(request, 'app/home.html')
 
@@ -18,9 +19,6 @@ class ProductView(View):
         return render(request , 'app/home.html' , context)
 
 
-# def product_detail(request):
-#  return render(request, 'app/productdetail.html')
-
 class product_detail(View):
     def get(self, request , pk):
         product = Product.objects.get(pk=pk)
@@ -33,11 +31,39 @@ def add_to_cart(request):
 def buy_now(request):
  return render(request, 'app/buynow.html')
 
-def profile(request):
- return render(request, 'app/profile.html')
+# class ProfileView(View):
+#     def get(self, request):
+#         form = CustomerProfileForm()
+#         return render(request, 'app/profile.html' , {'form' : form, 'active' : 'btn-primary'})
+
+class ProfileView(View):
+	def get(self, request):
+         form = CustomerProfileForm()
+         return render(request, 'app/profile.html' , {'form' : form, 'active' : 'btn-primary'})
+		
+	def post(self, request):
+		form = CustomerProfileForm(request.POST)
+		if form.is_valid():
+			usr = request.user
+			name  = form.cleaned_data['name']
+			locality = form.cleaned_data['locality']
+			city = form.cleaned_data['city']
+			state = form.cleaned_data['state']
+			zipcode = form.cleaned_data['zipcode']
+			reg = Customer(user=usr, name=name, locality=locality, city=city, state=state, zipcode=zipcode)
+			reg.save()
+			messages.success(request, 'Congratulations!! Profile Updated Successfully.')
+		return render(request, 'app/profile.html', {'form':form, 'active':'btn-primary'})
+
+
+
+
+
+
 
 def address(request):
- return render(request, 'app/address.html')
+    details= Customer.objects.filter(user= request.user)
+    return render(request, 'app/address.html' , {'details' : details , 'active' : 'btn-primary'}) 
 
 def orders(request):
  return render(request, 'app/orders.html')
@@ -52,11 +78,8 @@ def mobile(request, data=None):
         mobiles= Product.objects.filter(category= 'M').filter(brand=data)
     return render(request, 'app/mobile.html' , {'mobiles' :mobiles})
 
-# def login(request):
-#  return render(request, 'app/login.html')
-
-# def customerregistration(request):
-#  return render(request, 'app/customerregistration.html')
+def login(request):
+ return render(request, 'app/login.html')
 
 class customerregistration(View):
     def get(self, request):
@@ -68,10 +91,11 @@ class customerregistration(View):
         if form.is_valid():
             messages.success(request, 'Account has been registered!!')
             form.save()
-            return render(request, 'app/login.html')
-        else:
-            context = {'form' : form}
-            return render(request, 'app/customerregistration.html' , context )
+            return render(request, 'app/customerregistration.html')
+
+        context = {'form' : form}
+        return render(request, 'app/customerregistration.html' , context )
+        
         
 
 
